1dc467652395ca3bce64784aa7655d535cd22dfc
developer <128512807+wdarpan@users.noreply.github.com>
2023-12-21 22:12:15 +0530

    Personal portfolio 2024

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+var mixer = mixitup(".portfolio-gallery");
+
+// script swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,   },
+    });
